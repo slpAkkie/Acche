3535d48d8ac36182cca24486bc85d1a1f4dea77d
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-14 19:26:30 +0300

    Update: Overlay

--- resources/js/Components/Overlay.vue
+++ resources/js/Components/Overlay.vue
@@ -1,6 +1,6 @@
 <template>
     <teleport to="body">
-        <div class="bg-slate-800 opacity-50 absolute inset-0 z-90"></div>
+        <div class="bg-zinc-800 opacity-50 absolute inset-0 z-90"></div>
     </teleport>
 </template>
 
